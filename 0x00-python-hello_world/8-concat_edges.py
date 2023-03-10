62a93dd1db5615d0dcb7c31808c68fa46d36bb4b
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2023-03-10 00:06:40 +0000

    created a python file

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = str[str.find("object-oriented programming"):].split()[0]+" " + str[\
+str.find('with'):].split()[0] + " " +str[str.find('Python'):].split()[0]
 print(str)
