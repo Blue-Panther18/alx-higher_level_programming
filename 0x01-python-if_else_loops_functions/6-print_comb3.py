fc8af59c4a7abedd6d3f4403d0d321bd534450a3
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2023-03-14 23:43:45 +0000

    added a new python file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for n in range(10):
     for i in range(10):
         if n != i and i > n:
-            print("{}{}".format(n,i), end="")
+            print("{}{}".format(n, i), end="")
             if n != 8:
                 print(", ", end="")
 print("")
