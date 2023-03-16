7da99719e1d3a73e06abcdc3a2646cc2ef99b303
Abdusshakur Olabisi <olabisiabdusshakur01@gmail.com>
2023-03-16 21:57:48 +0000

    modified this file

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -7,15 +7,9 @@ def pow(a, b):
         n = b * -1
     else:
         n = b
-    if a < 0:
-        p = a * -1
-    else:
-        p = a
     for i in range(n):
         if b < 0:
-            result = result / p
+            result = result / a
         else:
-            result = result * p
-        if a < 0 and n % 2 == 1:
-            result = result * -1
+            result = result * a
     return result
